d90996727a47491f614f5e4453d1d07a3882450d
Sander Aarts <sanderaarts19992@gmail.com>
2021-02-25 15:15:19 +0100

    Layout update
    Buttons in bovenste balk toegevoegd, en naam verbeterd.

--- QuizApp/QuizApp/Controllers/HomeController.cs
+++ QuizApp/QuizApp/Controllers/HomeController.cs
@@ -13,16 +13,14 @@ namespace QuizApp.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult QuizPage()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Tutorial()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
