f947c76d38502dc0a74e05ea56dce6a9f061795f
Sander Aarts <sanderaarts19992@gmail.com>
2021-04-08 11:39:49 +0200

    Bootstrap and scorepage update
    - bootstrap update
    - scorepage is now dynamic

--- QuizApp/QuizApp/Controllers/ScoreboardController.cs
+++ QuizApp/QuizApp/Controllers/ScoreboardController.cs
@@ -9,20 +9,23 @@ namespace QuizApp.Controllers
     public class ScoreboardController : Controller
     {
         // GET: Scoreboard
-        public ActionResult Scoreboard(/*int category, string difficulty, string timespan*/)
+        public ActionResult Scoreboard(ScoreBoardModel scoreBoardModel)
         {
             if (Session["Login"] != null)
             {
                 int count = 0;
                 string scoreboard = "";
-                List<string> results = Load_Scoreboard.Get_ScoreBoard_Data("medium", 15);
+                List<string> results = Load_Scoreboard.Get_ScoreBoard_Data(Convert.ToString(scoreBoardModel.difficulty), (int)scoreBoardModel.category);
                 for (int i = 0; i < results.Count; i++)
                 {
-                    if(i <1)
+                    if (i < 1)
                     {
-                        scoreboard = (string)results[i]
+                        scoreboard = (string)results[i];
                     }
-                    scoreboard = scoreboard+","+(string)results[i];
+                    else
+                    {
+                        scoreboard = scoreboard + "," + (string)results[i];
+                    }
                     count = i;
                 }
                 ViewData["scoreboard"] = scoreboard;
